7716a9177affc8014429163d5142eab969c9ede8
Daniel Klose <daniel-stefan.klose@udo.edu>
2026-05-14 13:54:47 +0200

    feat: add Divider TSX component

---
+++ src/components/layout/Divider.tsx
@@ -0,0 +1,13 @@
+export default function Divider() {
+  return (
+    <div className="divider" aria-hidden="true">
+      <div className="dots">
+        <span className="dot d1" />
+        <span className="dot d2" />
+        <span className="dot d3" />
+        <span className="dot d4" />
+        <span className="dot d5" />
+      </div>
+    </div>
+  );
+}
